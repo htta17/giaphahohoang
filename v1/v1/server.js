165a2c344a2de37fe7591c9ad1800bd0c9a1715a
Anh Hoang <htta17@gmail.com>
2018-12-02 22:39:24 -0800

    Express  + Connect

--- v1/v1/server.js
+++ v1/v1/server.js
@@ -1,8 +1,17 @@
 'use strict';
-var http = require('http');
+var http = require('http'),
+    connect = require("connect");
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var app = connect()        
+    .use(function (req, res, next) {
+        console.log("First middleware");
+        next();
+    })
+    .use(function (req, res, next) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World\n Test');
+    })
+
+http.createServer(app).listen(3000);
+
