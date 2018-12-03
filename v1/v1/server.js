c911fbbe928723b608d23f12a987dbdaa08d7320
Anh Hoang <htta17@gmail.com>
2018-12-02 23:30:48 -0800

    Node Module

--- v1/v1/server.js
+++ v1/v1/server.js
@@ -1,17 +1,7 @@
-'use strict';
-var http = require('http'),
-    connect = require("connect");
-var port = process.env.PORT || 1337;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var app = connect()        
-    .use(function (req, res, next) {
-        console.log("First middleware");
-        next();
-    })
-    .use(function (req, res, next) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World\n Test');
-    })
+app.get('/', (req, res) => res.send('Hello World!'));
 
-http.createServer(app).listen(3000);
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
